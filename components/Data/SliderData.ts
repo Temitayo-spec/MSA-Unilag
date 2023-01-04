47ca72d2953cdbe000cafcdd10bd3b8dc7c52072
olawanle temitayo <olawanletemitayo@gmail.com>
2023-01-04 17:45:56 +0100

    fix: slider issue

--- components/Data/SliderData.ts
+++ components/Data/SliderData.ts
@@ -1,30 +1,30 @@
 export const SliderData = [
   {
-    image: '/images/banner-one.jpg',
+    image: '/images/banner-one.JPG',
     alt: 'banner-one',
   },
   {
-    image: '/images/banner-two.jpg',
+    image: '/images/banner-two.JPG',
     alt: 'banner-two',
   },
   {
-    image: '/images/banner-three.jpg',
+    image: '/images/banner-three.JPG',
     alt: 'banner-three',
   },
   {
-    image: '/images/banner-four.jpg',
+    image: '/images/banner-four.JPG',
     alt: 'banner-four',
   },
   {
-    image: '/images/banner-five.jpg',
+    image: '/images/banner-five.JPG',
     alt: 'banner-five',
   },
   {
-    image: '/images/banner-six.jpg',
+    image: '/images/banner-six.JPG',
     alt: 'banner-six',
   },
   {
-    image: '/images/banner-seven.jpg',
+    image: '/images/banner-seven.JPG',
     alt: 'banner-seven',
   },
 ];
