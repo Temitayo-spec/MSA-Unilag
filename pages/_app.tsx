549388d11917dbe49fd9020944c6f3209203ac70
olawanle temitayo <olawanletemitayo@gmail.com>
2023-01-04 16:28:25 +0100

    fix: moved to the stable version of Nextjs13

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,30 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { useEffect, useState } from 'react';
+import Preloader from '../components/Preloader';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [preloader, setPreloader] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPreloader(false);
+    }, 7000);
+  }, []);
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
+      {
+        preloader ? (
+          <Preloader />
+        ) : (
+          <>
+            <Header />
+            <Component {...pageProps} />
+            <Footer />
+          </>
+        )
+      }
     </>
   );
 }
